1dc20759b264fdf60f8381d9ad8a79f1d599eef5
Sahil Adit <aditsahil17@gmail.com>
2025-03-22 19:43:43 +0530

    page2 styles updated

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,10 +7,10 @@ const Card = ({ head, text }) => {
   initial={{ opacity: 0, y: 15 }} 
   whileInView={{ opacity: 1, y: 0 }} 
   transition={{ duration: 1 }} 
-  className="relative h-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/4 bg-[#373f51] text-white top-20 p-4 md:p-6 rounded-md"
+  className="relative h-auto w-full sm:w-3/4 md:w-1/3 md:top-52 lg:w-full bg-[#373f51] text-white top-20 p-4 md:p-6 rounded-md"
 >
-  <h2 className="font-bold text-lg md:text-xl">{head}</h2>
-  <p className="text-sm md:text-base mt-4">{text}</p>
+  <h2 className="font-bold text-lg  md:text-3xl">{head}</h2>
+  <p className="text-sm md:text-xl mt-4">{text}</p>
 </motion.div>
 
     );
